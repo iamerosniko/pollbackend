6e1d5df70de06f9277f1703c534999ed5d0fd0d9
Eros Niko Cas Alvarez <iamerosniko@yahoo.com>
2024-10-20 01:41:51 +0800

    changes v1

--- server.js
+++ server.js
@@ -1,54 +1,68 @@
 const express = require('express');
 const http = require('http');
+const path = require('path');
 const socketIo = require('socket.io');
 const cors = require('cors');
 
 const app = express();
 
-// Enable CORS for all origins
+// Enable CORS
 app.use(cors());
+
+// Serve Angular static files
+app.use(express.static(path.join(__dirname, 'dist/va')));
 
 const server = http.createServer(app);
 const io = socketIo(server, {
   cors: {
-    origin: "*", // Allow all origins
+    origin: "*", // Allow all origins for Socket.io
     methods: ["GET", "POST"],
   }
 });
 
-// Sample data for individuals
+// Socket.io logic here
 let individuals = [
 ];
 
-// Socket.io logic
 io.on('connection', (socket) => {
   console.log('New client connected');
 
   // Send initial list of individuals
   socket.emit('individuals', individuals);
 
-  // Admin adds a new individual
   socket.on('addIndividual', (data) => {
     individuals.push({ name: data.name, gender: data.gender, votes: 0 });
-    io.emit('individuals', individuals); // Broadcast updated list to all clients
+    io.emit('individuals', individuals);
   });
 
-  // User votes for an individual
   socket.on('vote', (name) => {
     const person = individuals.find(ind => ind.name === name);
     if (person) {
       person.votes += 1;
-      io.emit('individuals', individuals); // Broadcast updated list to all clients
+      io.emit('individuals', individuals);
     }
   });
+
+  socket.on('reset', (val) => {
+    console.log('reset');
+    individuals=[];
+    io.emit('individuals',individuals);
+  })
 
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
 });
 
+
+
+// Redirect all other routes to Angular app (for routing)
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'dist/va/index.html'));
+});
+
 // Start the server
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 server.listen(PORT, () => {
   console.log(`Server running on port ${PORT}`);
 });
